d929c246f1c081b3ce03965558a41c1afcf8d078
ybbat <cameron@cgst.info>
2023-12-02 09:57:22 +0000

    Day 2

--- src/day2/a.cpp
+++ src/day2/a.cpp
@@ -1,6 +1,50 @@
+#include <cmath>
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello, World! 3" << std::endl;
+    std::string line;
+    int acc = 0;
+
+    int game = 1;
+    while (std::getline(std::cin, line))
+    {
+        bool valid = true;
+        int i = 0;
+        while (line[i] != ':') i++; // skip until real data
+
+        for (; i < line.size(); i++)
+        {
+            int j = 0;
+            while (isdigit(line[i+j])) j++;
+
+            int num = 0;
+            for (int k = 0; j-k > 0; k++) {
+                num += (line[i+j-k-1] - '0') * pow(10, k);
+            }
+
+            if (num != 0) {
+                switch (line[i+j+1]) {
+                    case 'r':
+                        if (num > 12) valid = false;
+                        break;
+                    case 'g':
+                        if (num > 13) valid = false;
+                        break;
+                    case 'b':
+                        if (num > 14) valid = false;
+                        break;
+                }
+            }
+
+            if (valid == false) {
+                break;
+            }
+        }
+
+        if (valid) acc += game;
+        game++;
+    }
+
+    std::cout << acc << std::endl;
 }
--- src/day2/b.cpp
+++ src/day2/b.cpp
@@ -1,6 +1,48 @@
+#include <cmath>
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello, World! 4" << std::endl;
+    std::string line;
+    int acc = 0;
+
+    int game = 1;
+    while (std::getline(std::cin, line))
+    {
+        int maxr = 0, maxg = 0, maxb = 0;
+
+        int i = 0;
+        while (line[i] != ':') i++; // skip until real data
+
+        for (; i < line.size(); i++)
+        {
+            int j = 0;
+            while (isdigit(line[i+j])) j++;
+
+            int num = 0;
+            for (int k = 0; j-k > 0; k++) {
+                num += (line[i+j-k-1] - '0') * pow(10, k);
+            }
+
+            if (num != 0) {
+                switch (line[i+j+1]) {
+                    case 'r':
+                        maxr = std::max(maxr, num);
+                        break;
+                    case 'g':
+                        maxg = std::max(maxg, num);
+                        break;
+                    case 'b':
+                        maxb = std::max(maxb, num);
+                        break;
+                }
+            }
+        }
+
+        acc += maxr * maxg * maxb;
+
+        game++;
+    }
+
+    std::cout << acc << std::endl;
 }
